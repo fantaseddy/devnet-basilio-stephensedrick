16fe5b65caed5f1fe04aec86f6eaf9be85fe5a19
Stephen Sedrick C. Basilio <storytellerseddy@gmail.com>
2026-09-23 22:18:29 +0800

    Finished Lesson 1

--- module-2-python-basics/lesson-1-variables-datatypes.py
+++ module-2-python-basics/lesson-1-variables-datatypes.py
@@ -7,20 +7,21 @@ Date: September 23, 2026
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
-[write your own explanation here]
+Variables are used to store information in a program so we can use it later. For example, if I want to store my name = Seddy, age = 23, or favorite_subject = DEVNET, I can put each value inside a variable and give it a name. :)
+A data type tells Python what kind of value is being stored: integers for whole numbers, floats for numbers with decimals, strings for text, and booleans for True or False values. Data type is important because we can add two integers together, but not int and str without converting the string first.
 
 
 ============================================
 KEY VOCABULARY
 ============================================
-- variable:
-- data type:
-- int:
-- float:
-- string:
-- boolean:
+- variable: container with a value
+- data type: what kind of value is in a variable
+- int: whole number w/o decimal
+- float: number w/ decimals
+- string: text inside double quotes
+- boolean: True or False value
 (add more as needed)
-
+- type conversion: changing a variable's data type to another type, often to perform operations
 
 ============================================
 MY OWN EXAMPLE(S)
@@ -29,19 +30,26 @@ Write at least one working example below that you
 came up with yourself — not copied from class.
 """
 
-# --- your code example goes here ---
+student_name = "Seddy"
+subjects = 7
+average_grade = 95
+is_passed = True
+
+print(f"Student: {student_name}")
+print(f"Subjects completed: {subjects_completed}")
+print(f"Average grade: {average_grade}")
+print(f"Passed: {is_passed}")
 
 
 """
 ============================================
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
-[what's something confusing or easy to get wrong
-about this topic?]
+I forgot to locally save again using Ctrl + S XD. I also often forget that inputs will consider it as string, so I must use int() to convert it first.
 
 
 ============================================
 HOW THIS CONNECTS TO SOMETHING ELSE
 ============================================
-[optional]
+Variables and data types are the building blocks of programming, especially Python. I need to declare variables in almost everything I do.
 """
