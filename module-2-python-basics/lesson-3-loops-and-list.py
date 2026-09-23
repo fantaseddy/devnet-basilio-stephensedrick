98d902d559ae2a1e19ca79563b1c205a8b70d788
Stephen Sedrick C. Basilio <storytellerseddy@gmail.com>
2026-09-23 22:40:26 +0800

    Started Lesson 3

--- module-2-python-basics/lesson-3-loops-and-list.py
+++ module-2-python-basics/lesson-3-loops-and-list.py
@@ -1,7 +1,7 @@
 """
 Module 2 — Lesson 3: Loops & Lists
-Student: [your name]
-Date: [date]
+Student: Stephen Sedrick C. Basilio
+Date: September 23, 2026
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
